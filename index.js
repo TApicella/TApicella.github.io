adddec08b09db6430372949be0dfbe2c31351fbe
Thomas Apicella <tapicella.ms@gmail.com>
2015-02-17 14:33:41 -0500

    add button test js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+$(document).ready(function(){
+	$('#myButton').on('click', function () {
+    var $btn = $(this).button('loading');
+    // business logic...
+    $btn.button('reset');
+  });
+});
