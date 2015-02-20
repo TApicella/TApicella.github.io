afa8cf7e2f9e50c4dc1617014b973679ad3a3c49
Thomas Apicella <tapicella.ms@gmail.com>
2015-02-20 12:17:22 -0500

    Testhing weather api

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-$(document).ready(function(){
-	$('#myButton').on('click', function () {
-    var $btn = $(this).button('loading');
-    // business logic...
-    $btn.button('reset');
-  });
+$(document).ready(function() {
+    //Weather capturing from http://www.onextrapixel.com/2011/08/22/adding-weather-to-your-site-with-jquery-and-yql/
+
+    //Setup
+    var zipcode = '19145';
+    var units = 'f';
+
+    //Query
+    var query = "SELECT item.condition FROM weather.forecast WHERE location='" + zipcode + "' AND u='" + units + "'";
+    var cacheBuster = Math.floor((new Date().getTime()) / 1200 / 1000);
+    var url = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent(query) + '&format=json&_nocache=' + cacheBuster;
+
+    //Callback
+    window['wCallback'] = function(data) {
+        var info = data.query.results.channel.item.condition;
+        alert(info.text);
+    };
+
+    //Ajax call
+    $.ajax({
+        url: url,
+        dataType: 'jsonp',
+        cache: true,
+        jsonpCallback: 'wCallback'
+    });
 });
