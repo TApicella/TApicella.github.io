66121064426fae0e0f05ec089429f2dfe88861f7
Thomas Apicella <tapicella.ms@gmail.com>
2015-02-23 17:11:53 -0500

    First draft thunder theme

--- index.js
+++ index.js
@@ -1,20 +1,64 @@
-$(document).ready(function() {
-	//Fixing grid
-	var spacing_string = "";
-	for(var i = 0; i < 100; i++){
-		spacing_string+="A<br>";
-	}
-	$("[class$='b']").each(function(){
-		$(this).html(spacing_string);
-	});
+function makeitrain() {
+    var black = "#000000"; //Weight 66/150
+    var dBlue = "#060023"; //Weight 66/150
+    var medBlue = "#020069"; //Weight 8/150
+    var brightBlue = "#0318A2"; //Weight 8/150
+    var yellow = "#FAE212"; //Weight 1/150
 
-	//Weather capturing from http://www.onextrapixel.com/2011/08/22/adding-weather-to-your-site-with-jquery-and-yql/
+    var stdWidth = 4;
+    var rainWidth = 2;
+    var totalWidth = 0;
+
+    var resultcss = "linear-gradient(90deg";
+    while (totalWidth < 150) {
+        var next = Math.floor((Math.random() * 150) + 1);
+        if (next > 0 && next <= 66) {
+            totalWidth += stdWidth;
+            resultcss += ", " + black + " " + totalWidth + "px";
+
+        } else if (next > 66 && next <= 132) {
+            totalWidth += stdWidth;
+            resultcss += ", " + dBlue + " " + totalWidth + "px";
+        } else if (next > 132 && next <= 140) {
+            totalWidth += rainWidth;
+            resultcss += ", " + medBlue + " " + totalWidth + "px";
+        } else if (next > 140 && next <= 149) {
+            totalWidth += rainWidth;
+            resultcss += ", " + brightBlue + " " + totalWidth + "px";
+        } else {
+            totalWidth += rainWidth;
+            resultcss += ", " + yellow + " " + totalWidth + "px";
+        }
+    }
+    resultcss += ")";
+
+    return resultcss;
+}
+
+function setupColumns() {
+    $("[class*='thunder-border']").height($(window).height());
+    $("[class*='main-content']").height($(window).height());
+    $("[class*='thunder-border']").each(function() {
+        $(this).css({
+            "background-image": makeitrain()
+        });
+    });
+}
+
+$(document).ready(function() {
+    //Fixing columns
+    setupColumns();
+    $(window).resize(function() {
+        setupColumns();
+    });
+
+    //Weather capturing from http://www.onextrapixel.com/2011/08/22/adding-weather-to-your-site-with-jquery-and-yql/
     //Also using SunCalc: https://github.com/mourner/suncalc
 
     //Setup
     var zipcode = '19145';
     var latitude = 39.9149;
-    var longitude =	75.1911;
+    var longitude = 75.1911;
     var units = 'f';
     var date = new Date();
 
